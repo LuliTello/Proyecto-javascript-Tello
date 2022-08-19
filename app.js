49add896870c7510a547890a59092c8215257ce4
Lucia Tello <tellolucia87@gmail.com>
2022-08-19 11:01:37 -0300

    desafio bucle for, while, do while

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* CONDICIONAL
 let pasaje = 7000;
 let pasaje2 = 5500;
 let destino = prompt('Ingrese la opcion de destino Cordoba o Santa Fe');
@@ -20,8 +21,39 @@ if(( pasajero == 'Adulto' || pasajero == 'adulto' || pasajero == 'ADULTO') && (d
 
 }else if(( pasajero == 'Jubilado' || pasajero == 'jubilado' || pasajero == 'JUBILADO') && (destino == 'Santa Fe' || destino == 'santa fe' || destino == 'SANTA FE')){
     alert('El costo de su pasajero es de $' + ' ' + (pasaje2/1.15));  
-      
+
 }else{
     alert('Error: Ingrese alguna de las opciones');
-}
+}*/
 
+//BUCLE
+
+/*let destino1 = 'cordoba';
+let destino2 = 'santa fe';
+let destino3 = 'buenos aires';
+let compraPasaje = prompt('escriba en minusculas a donde quiere ir');
+
+while(compraPasaje !== destino1 && compraPasaje !== destino2 && compraPasaje !== destino3){
+    alert('Usted no puede comprar su pasaje');
+    compraPasaje = prompt('escriba en minusculas a donde quiere ir');
+}*/
+
+//otro bucle con while
+let ingresar = false;
+let usuario = 'lucia';
+let contraseña = 'contraseña';
+let contador = 0;
+
+
+while(contador < 3 && ingresar === false){
+    let palabraUsuario = prompt('ingrese su usuario');
+    let palabraContraseña = prompt('ingrese su contraseña');
+    if( palabraUsuario === usuario && palabraContraseña === contraseña ){
+        ingresar = true;
+        alert('usted ha ingresado correctamente');
+    }
+    contador++;
+}
+if (contador == 3 || !ingresar){
+alert('usted supero los intentos para ingresar usuario y contraseña');
+}
