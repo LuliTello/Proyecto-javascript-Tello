5b204028dfc53c90992437b1b8fcec897a5adfe3
Lucia Tello <tellolucia87@gmail.com>
2022-09-14 10:20:49 -0300

    agregando funcion de pago y mejorando html

--- app.js
+++ app.js
@@ -23,36 +23,6 @@ let niño = 0;
 let pago = false;
 let cuota;
 
-//funcion para dar bienvenida
-const solicitarNombre = () => {
-    alert('Usted ha ingresado a la plataforma BulletTrain para viajar seguro y rápido');
-    let nombre = prompt('Por favor ingrese su nombre');
-    while (nombre === '' || (!isNaN(nombre))) {
-        nombre = prompt('Por favor ingrese su nombre');
-    }
-    //muestro al cliente
-    alert('Hola' + ' ' + nombre.toUpperCase() + ' ' + 'bienvenido/a!, por favor registrate para comprar con nosotros');
-}
-//llamada a la funcion
-solicitarNombre();
-
-//funcion para ingresar
-
-const ingresoUsuario = () => {
-    let usuario = prompt("Ingresa tu usuario para continuar");
-    let password = prompt("Ingresa tu contraseña para continuar");
-
-    while (usuario === '' || password === '') {
-        usuario = prompt("Ingresa tu usuario para continuar");
-        password = prompt("Ingresa tu contraseña para continuar");
-    }
-    //muestro al usuario
-    alert(usuario.toUpperCase() + ' ' + 'ingresaste correctamente');
-};
-ingresoUsuario();
-
-//filtro por origen y destino en  array con la informacion de los pasajes
-
 const tramos = [
 
     { from: 'Buenos Aires', to: 'Cordoba', at: '13:00', price: 3500 },
@@ -69,94 +39,11 @@ const tramos = [
     { from: 'Santa Fe', to: 'Cordoba', at: '23:00', price: 2500 },
 ]
 
-//funcion para elegir origen
 
-const elegirOrigen = () => {
-    while (elegir === false) {
-        from = prompt('De donde quiere salir \n Buenos Aires \n Cordoba \n Santa Fe \n');
-        fromGuardado = from.toLowerCase();
-        if (fromGuardado === 'buenos aires' || fromGuardado === 'cordoba' || fromGuardado === 'santa fe') {
-            elegir = true;
-
-            alert('Usted ha seleccionado origen ' + fromGuardado);
-
-            console.log(fromGuardado)
-            return (fromGuardado)
-        } else {
-            alert('seleccione una opcion valida');
-        }
-    }
-}
-elegirOrigen();
-
-//funcion para elegir destino
-
-const elegirDestino = () => {
-    while (continuar === false) {
-        to = prompt('A donde quiere dirigirse sin repetir origen \n Buenos Aires \n Cordoba \n Santa Fe');
-        toGuardado = to.toLowerCase();
-
-        if ((toGuardado === 'buenos aires' || toGuardado === 'cordoba' || toGuardado === 'santa fe') && toGuardado !== fromGuardado) {
-            continuar = true;
-
-            alert('Usted ha seleccionado destino ' + toGuardado);
-            console.log(toGuardado)
-            return (toGuardado)
-        } else {
-            alert('seleccione una opcion valida');
-        }
-    }
-}
-elegirDestino();
-
-//metodo filter para filtrar por destino y origenlet filtrado;
-const filtrarPasaje = (arr, fromGuardado, toGuardado) => {
-    filtrado = arr.filter((el) => {
-
-        return el.from.toLowerCase() === fromGuardado && el.to.toLowerCase() === toGuardado;
-    });
-
-    return filtrado
-}
-const resultados = filtrarPasaje(tramos, fromGuardado, toGuardado);
-console.log(resultados);
-
-alert(`Ahora que ya conoce el origen y destino puede elegir un horario`);
-
-//funcion para elegir horario
-
-const validarHorario = () => {
-    while (validar === false) {
-        horario = prompt('Elija el horario en el que desea salir \n  13:00 \n  23:00 ');
-
-        if (horario === '13:00' || horario === '23:00') {
-            validar = true;
-
-            alert('Usted ha seleccionado horario' + horario);
-            console.log(horario)
-            return horario
-        } else {
-            alert('seleccione una opcion valida');
-        }
-    }
-}
-validarHorario();
-
-//metodo find para elegir el horario deseado
-
-const buscarHorario = (arr, horario) => {
-    pasajeEncontrado = arr.find((el) => {
-        return el.at === horario
-    });
-    return pasajeEncontrado;
-}
-pasajeEncontrado = buscarHorario(resultados, horario);
-console.log(pasajeEncontrado);
-alert(`Usted ha elegido ${pasajeEncontrado.from} por un valor de $ ${pasajeEncontrado.price}`);
 //Array vacio donde iran los pasajes comprados
 const carrito = [];
 
-class destino {
+class destinoViaje {
     constructor(desde, hasta, precio) {
         this.from = desde.toUpperCase();
         this.to = hasta.toUpperCase();
@@ -165,69 +52,8 @@ class destino {
 
     }
 }
-carrito.push(pasajeEncontrado);
-console.log(carrito);
 
-//calculo de IVA usando MAP
-const precioIva = carrito.map((el) => {
-    return {
-        from: el.from,
-        to: el.to,
-        at: el.at,
-        price: el.price * 1.21
-    }
-})
-console.log(precioIva);
-
-
-//total compra usando reduce
-const total = precioIva.reduce((acc, el) => {
-    return acc + el.price;
-}, 0);
-
-alert('El precio de su reserva es de $' + ' ' + Math.round(total) + ' ' + 'FINAL');
-console.log(precioIva);
-
-//funcion para comprar pasaje segun cantidad pasajeros
-
-const calcularPasaje = () => {
-
-    while (calcular === false) {
-
-        cantidad = parseInt(prompt('Ingrese un número de pasajeros'));
-
-        if (!isNaN(cantidad)) {
-            calcular = true;
-
-            precioTramo = total * cantidad;
-
-            alert('el precio de su pasaje es de: $' + precioTramo + ' ' + 'FINAL');
-            return precioTramo;
-
-
-        } else {
-            calcular = false;
-            alert('Ha elegido una opcion invalida');
-
-        }
-    }
-}
-
-//creacion de variable global y llamada a funcion
-let precioPasaje = calcularPasaje(precioTramo);
-//nuevo array segun cantidad de pasajeros
-const precioCantidad = precioIva.map((el) => {
-    return {
-        from: el.from,
-        to: el.to,
-        at: el.at,
-        price: el.price * cantidad
-    }
-})
-console.log(precioCantidad);
-//funcion descuento por tipo pasajero
-
-const descuentoPasaje = () => {
+/*const descuentoPasaje = () => {
     while (pasajero === false) {
         let tipoPasajero = parseInt(prompt('Seleccione la opción de tipo de pasajero \n 1-Adulto \n 2-Niño \n 3-Jubilado'));
 
@@ -303,9 +129,207 @@ const seleccionPago = () => {
 
 }
 //llamada funcion
-seleccionPago(totalPagar);
+seleccionPago(totalPagar);*/
 
 //DOM
+
+const btnUsuario = document.querySelector('.btn__ingresar')
+const header = document.querySelector('header')
+// evento click para ingreso de usuario
+btnUsuario.addEventListener('click', () => {
+    const nombre = document.querySelector('.nombreUsuario__input').value
+    const contraseña = document.querySelector('.contraseñaUsuario__input').value
+
+    console.log(nombre);
+    console.log(contraseña);
+    const ingresoUsuarioYContraseña = () => {
+        if ((nombre === '' || (!isNaN(nombre))) || contraseña === '') {
+            let p = document.createElement('p')
+            p.innerText = 'Ingrese Usuario y contraseñas validas'
+            p.classList.add('fallo__ingreso')
+            header.append(p);
+        } else {
+            let p = document.createElement('p')
+            p.innerText = nombre.toUpperCase() + ' ha ingresado correctamente, proceda a hacer su reserva'
+            p.classList.add('valido__ingreso')
+            header.append(p);
+        }
+    }
+    ingresoUsuarioYContraseña();
+})
+// evento change para seleccionar origen
+const selectOrigen = document.querySelector('#origen')
+
+selectOrigen.addEventListener('change', () => {
+    from = selectOrigen.value
+    console.log(from);
+    const elegirOrigen = () => {
+        if ((from === 'buenos aires' || from === 'cordoba' || from === 'santa fe') && from !== destino) {
+            document.querySelector('#origenSeleccionado').innerText = `Usted ha seleccionado origen ${from}`;
+        } else {
+            document.querySelector('#origenSeleccionado').innerText = 'Debe seleccionar un Origen para continuar'
+        }
+    }
+    elegirOrigen();
+})
+// evento change para seleccionar destino
+const selectDestino = document.querySelector('#destino')
+selectDestino.addEventListener('change', () => {
+    to = selectDestino.value;
+    console.log(to);
+    const elegirDestino = () => {
+        if ((to === 'buenos aires' || to === 'cordoba' || to === 'santa fe') && to !== from) {
+            document.querySelector('#destinoSeleccionado').innerText = `Usted ha seleccionado origen ${to}`;
+        } else {
+            document.querySelector('#destinoSeleccionado').innerText = 'Debe seleccionar un destino para continuar'
+        }
+    }
+    elegirDestino();
+
+})
+
+// evento change para seleccionar horario
+
+const selectHorario = document.querySelector('#horario')
+selectHorario.addEventListener('change', () => {
+    horario = selectHorario.value;
+    console.log(horario);
+    const elegirHorario = () => {
+        if (horario === '13:00' || horario === '23:00') {
+            document.querySelector('#horarioSeleccionado').innerText = `Usted eligió el horario ${horario}`;
+        } else {
+            document.querySelector('#horarioSeleccionado').innerText = 'Debe seleccionar un horario para continuar'
+        }
+    }
+    elegirHorario();
+})
+//buscar pasaje con parametros seleccionados
+let reservados;
+const buscar = document.querySelector('#buscar')
+buscar.addEventListener('click', () => {
+
+    const filtrarPasaje = (arr, from, to, horario) => {
+        filtrado = arr.filter((el) => {
+
+            return el.from.toLowerCase() === from && el.to.toLowerCase() === to && el.at === horario;
+        });
+
+        return filtrado
+    }
+    reservados = filtrarPasaje(tramos, from, to, horario);
+    console.log(reservados);
+
+    const formulario = document.querySelector('.formulario')
+    for (const resultado of reservados) {
+        let span = document.createElement('span')
+        span.innerText = `Resultado busqueda origen desde ${resultado.from} y destino ${resultado.to} en el horario de ${resultado.at} tiene un costo de $ ${resultado.price}`
+        span.classList.add('resultado__compra')
+        formulario.appendChild(span)
+    }
+})
+//agregar al carrito
+
+const reservas = [];
+const contenedorReserva = document.querySelector('.reservas')
+const agregar = document.querySelector('#agregar')
+agregar.addEventListener('click', () => {
+
+    for (const elemento of reservados) {
+        reservas.push(elemento);
+
+        console.log(reservas);
+    let div = document.createElement('div')
+    div.innerHTML = `
+    <h4> ${elemento.from} - ${elemento.to} </h4>
+    <p> ${elemento.at} Hs</p>
+    <h5> $ ${elemento.price} /persona</h5>`
+     div.classList.add('card-reserva')   
+       
+     contenedorReserva.append(div)
+    }
+})
+
+//sumar total de reserva para comprar
+let totalCompra;
+const section = document.querySelector('.reserva')
+const comprar = document.querySelector('#comprar')
+comprar.addEventListener('click', ()=>{
+    
+totalCompra = reservas.reduce((acc, el) => {
+    return acc + el.price;
+}, 0);
+
+console.log(totalCompra);
+
+let span = document.createElement('span')
+span.innerText= `El total de su compra es de $ ${totalCompra} + IVA, seleccione su forma de pago`
+span.classList.add('compra_span')
+section.append(span);
+})
+//finalizar compra
+/*const finalizar = document.querySelector('#finalizar')
+compraFinalizada = false;
+finalizar.addEventListener('click', () =>{
+   
+    
+})*/
+//forma de pago
+//forma de pago
+
+let pagar;
+let totalPagar;
+const formaPago = document.querySelector('#formaPago')
+formaPago.addEventListener('change', ()=>{
+
+    pagar = formaPago.value.toLowerCase()
+    console.log(pagar)
+
+    const seleccionPago = () => {
+        if(pagar==='debito'){
+            totalPagar = (totalCompra * 1.21) * 0.85;
+            document.querySelector('#pagoSeleccionado').innerText = `Usted eligió la forma de pago ${pagar} con un descuento del 15%, su compra total es de ${totalPagar} Final`;
+        }else if (pagar === 'transferencia'){
+            totalPagar = (totalCompra * 1.21) * 0.85;
+            document.querySelector('#pagoSeleccionado').innerText = `Usted eligió la forma de pago ${pagar} con un descuento del 15%, su compra total es de ${totalPagar} Final`;
+        }else{
+            totalPagar = totalCompra *1.21;
+            document.querySelector('#pagoSeleccionado').innerText = `Usted eligió la forma de pago ${pagar} su compra total es de ${totalPagar} Final`;
+        }
+        
+        }
+        seleccionPago();
+})
+
+
+//change para seleccionar tipo pasajero
+const selectPasajero = document.querySelector('#pasajero')
+selectPasajero.addEventListener('change', () => {
+    pasajero = selectPasajero.value;
+    console.log(pasajero);
+
+    const elegirPasajero = () => {
+        if (pasajero === 'adulto' || pasajero === 'menor' || pasajero === 'jubilado') {
+            document.querySelector('#pasajeroSeleccionado').innerText = `Usted seleccionó tipo de pasajero ${pasajero}`;
+        } else {
+            document.querySelector('#pasajeroSeleccionado').innerText = 'por favor seleccione una opcion valida';
+        }
+    }
+    elegirPasajero();
+})
+
+//formulario de contacto
+/*const submit = document.querySelector('#submit')
+submit.addEventListener('submit', (e)=>{
+   
+    e.preventDefault();
+
+    let formulario =e.target
+    console.log(formulario[0].value)
+    console.log(formulario[1].value)
+    console.log(formulario[2].value)
+    console.log(formulario[3].value)
+})*/
+
 //agrego h2
 const h2 = document.getElementById('h2');
 h2.innerText = 'Servicios y Tarifas';
@@ -319,7 +343,6 @@ for (const viaje of tramos) {
     <h4> ${viaje.from} - ${viaje.to} </h4>
     <p> ${viaje.at} Hs</p>
     <h5> $ ${viaje.price} /persona</h5> 
-    <button class="card__link">Agregar</button>
     </div>`
 
     ul.append(li)
@@ -341,16 +364,7 @@ terminoClases[0].innerText = 'Los menores de 2 años no pagan pasaje y deben ir 
 terminoClases[1].innerText = 'Jubilados 15% de descuento.'
 terminoClases[2].innerText = 'La tarifa del pasaje incluye almuerzo o cena y un equipaje de mano.'
 
-//agrego texto para ver el resultado de la reserva de pasajes
-const formulario = document.querySelector('.formulario')
-for (const resultado of precioCantidad) {
-    let p = document.createElement('p')
-    p.innerHTML = `Usted ha comprado un pasaje de origen ${resultado.from} con destino a ${resultado.to} en el horario de ${resultado.at} Hs. por un precio de $ ${resultado.price} Final`
-    p.classList.add('resultado__compra')
-    formulario.appendChild(p)
-}
-
-//Modifico los value de input nombre
+/*//Modifico los value de input nombre
 const inputNombre = document.getElementById('nombre')
 inputNombre.value = 'Ingrese nombre y apellido';
 
@@ -361,7 +375,7 @@ const inputTel = document.getElementById('telefono')
 inputTel.value = 'Ingrese su telefono';
 
 const inputMsj = document.getElementById('consulta')
-inputMsj.value = 'Ingrese su consulta o reclamo';
+inputMsj.value = 'Ingrese su consulta o reclamo';*/
 
 //mensaje de salida una vez hecha la compra
 const p = document.createElement('p');
