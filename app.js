916298fd65a7b779f517825b751ed6e1671140e1
Lucia Tello <tellolucia87@gmail.com>
2022-09-19 16:04:56 -0300

    agregando clear para que se borre el localstorage

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 //Segunda entrega Proyecto final
 
 //variables
-let usuario;
-let password;
-let elegir = false;
 let from;
-let fromGuardado;
-let continuar = false;
 let to;
-let toGuardado;
 let filtrado;
-let validar = false;
 let horario;
-let encontrado;
-let calcular = false;
 let cantidad;
-let precioTramo;
-let pasajero = false;
-let adulto = 1;
-let jubilado = 0.85;
-let niño = 0;
 let cuota;
 let reservados;
 let totalCompra;
@@ -44,7 +30,6 @@ const tramos = [
 ]
 
 //clase para crear objetos
-
 class destinoViaje {
     constructor(desde, hasta, precio) {
         this.from = desde.toUpperCase();
@@ -88,9 +73,9 @@ btnUsuario.addEventListener('click', () => {
             header.append(p);
         }
     }
-    ingresoUsuarioYContraseña(); 
-})
+    ingresoUsuarioYContraseña();
 
+})
 // evento change para seleccionar origen
 const selectOrigen = document.querySelector('#origen')
 
@@ -106,7 +91,6 @@ selectOrigen.addEventListener('change', () => {
     }
     elegirOrigen();
 })
-
 // evento change para seleccionar destino
 const selectDestino = document.querySelector('#destino')
 selectDestino.addEventListener('change', () => {
@@ -162,45 +146,44 @@ buscar.addEventListener('click', () => {
         formulario.appendChild(span)
     }
 })
+//evento para agregar la reserva con click
 
-//array vacio donde van las reservas
 const reservas = JSON.parse(localStorage.getItem('reservas')) || [];
 const contenedorReserva = document.querySelector('.reservas')
-//evento para agregar la reserva con click
 const agregar = document.querySelector('#agregar')
 agregar.addEventListener('click', () => {
 
     for (const elemento of reservados) {
         //llamada a funcion agregar
         agregarReserva(elemento)
-        
+
     }
 })
 //funcion para agregar reserva al arreglo
-function agregarReserva(elemento){
-    let existe = reservas.some(elem=>elem.from===elemento.from && elem.to ===elemento.to);
-    if(existe===false){
-        elemento.cantidad =1;
+function agregarReserva(elemento) {
+    let existe = reservas.some(elem => elem.from === elemento.from && elem.to === elemento.to);
+    if (existe === false) {
+        elemento.cantidad = 1;
         reservas.push(elemento);
-    }else{
-        let elemFind = reservas.find(elem=>elem.from ===elemento.from && elem.to===elemento.to)
+    } else {
+        let elemFind = reservas.find(elem => elem.from === elemento.from && elem.to === elemento.to)
         elemFind.cantidad++;
-        
+
     }
     console.log(reservas)
     //llamada a funcion mostrar
     mostrarReserva();
 }
 //funcion mostrar reserva en formato cards
-function mostrarReserva(){
-    contenedorReserva.innerHTML="";
-    for(const elemento of reservas){
+function mostrarReserva() {
+    contenedorReserva.innerHTML = "";
+    for (const elemento of reservas) {
         contenedorReserva.innerHTML += `<div class="card-reserva">
         <h4> ${elemento.from} - ${elemento.to} </h4>
         <p> ${elemento.at} Hs</p>
         <h5> $ ${elemento.price} /persona</h5>
         <p>${elemento.cantidad}</p>
-        </div>`    
+        </div>`
     }
     //almacenamiento de reservas con localStorage
     localStorage.setItem('reservas', JSON.stringify(reservas));
@@ -210,103 +193,106 @@ const borrar = document.querySelector('#borrar')
 borrar.addEventListener('click', () => {
 
     for (const elemento of reservados) {
-        borrarReserva(elemento);
+        borrarCarrito(elemento);
     }
     console.log(reservas);
-    })
+})
 
-  function borrarReserva(elemento){
+function borrarCarrito(elemento) {
     const findIndex = reservas.indexOf(elemento);
     reservas.splice(findIndex, 1);
     mostrarReserva();
 
-  }  
+}
 //sumar total de reserva para comprar usando reduce y evento click
 
 const section = document.querySelector('.reserva')
 const comprar = document.querySelector('#comprar')
-comprar.addEventListener('click', ()=>{
-    
-totalCompra = reservas.reduce((acc, elemento) => {
-    return acc + parseInt(elemento.price*elemento.cantidad);
-}, 0);
+comprar.addEventListener('click', () => {
 
-console.log(parseInt(totalCompra));
+    totalCompra = reservas.reduce((acc, elemento) => {
+        return acc + parseInt(elemento.price * elemento.cantidad);
+    }, 0);
 
-let span = document.createElement('span')
-span.innerText= `El total de su compra es de $ ${totalCompra} + IVA, finalice su compra y seleccione su forma de pago`
-span.classList.add('compra_span')
-section.append(span);
+    console.log(parseInt(totalCompra));
+
+    let span = document.createElement('span')
+    span.innerText = `El total de su compra es de $ ${totalCompra} + IVA, finalice su compra y seleccione su forma de pago`
+    span.classList.add('compra_span')
+    section.append(span);
 })
 
 //forma de pago con evento change
 
 const formaPago = document.querySelector('#formaPago')
-formaPago.addEventListener('change', ()=>{
+formaPago.addEventListener('change', () => {
 
     pagar = formaPago.value.toLowerCase()
     console.log(pagar)
 
     const seleccionPago = () => {
-        if(pagar==='debito'){
+        if (pagar === 'debito') {
             totalPagar = (totalCompra * 1.21) * 0.85;
             document.querySelector('#pagoSeleccionado').innerText = `Usted eligió la forma de pago ${pagar} con un descuento del 15%, su compra total es de ${totalPagar} Final`;
-        }else if (pagar === 'transferencia'){
+        } else if (pagar === 'transferencia') {
             totalPagar = (totalCompra * 1.21) * 0.85;
             document.querySelector('#pagoSeleccionado').innerText = `Usted eligió la forma de pago ${pagar} con un descuento del 15%, su compra total es de ${totalPagar} Final`;
-        }else if(pagar === 'credito'){
-            totalPagar = totalCompra *1.21;
+        } else if (pagar === 'credito') {
+            totalPagar = totalCompra * 1.21;
             document.querySelector('#pagoSeleccionado').innerText = `Usted eligió la forma de pago ${pagar} su compra total es de ${totalPagar} Final, elija cantidad de cuotas a pagar`;
-            
-            //opcion de cuotas con credito
-            if(valorCuota === 'una'){
-                totalCuota= totalPagar/1;
-                document.querySelector('#cuotaSeleccionada').innerText = `Usted eligió 1 cuota sin interes, el valor de su cuota es de $ ${totalCuota}`;
-            }else if(valorCuota === 'tres'){
-                totalCuota = (totalPagar*1.25)/3;
-                document.querySelector('#cuotaSeleccionada').innerText = `Usted eligió 1 cuota sin interes, el valor de su cuota es de $ ${totalCuota}`;
-            }else{
-                totalCuota = (totalPagar*1.40)/6;
-                document.querySelector('#cuotaSeleccionada').innerText = `Usted eligió 1 cuota sin interes, el valor de su cuota es de $ ${totalCuota}`;
-            }
-            
-        }else{
-            document.querySelector('#pagoSeleccionado').innerText =`Seleccione una opcion valida`
-        }
-        
-        }
-        seleccionPago();
-        console.log(parseInt(totalPagar));
 
-        //mensaje despues de pagar
-        const pago = document.querySelector('#pago')
-        const p = document.createElement('p');
-        p.innerText = 'Gracias por realizar su pago, por favor complete el formulario de contacto para recibir nuestra informacion';
-        p.classList.add('saludo')
-        pago.append(p)
-    
+        } else {
+            document.querySelector('#pagoSeleccionado').innerText = `Seleccione una opcion valida`
+        }
+
+    }
+    seleccionPago();
+    console.log(parseInt(totalPagar));
+    // borrar almacenamiento una vez hecha la compra
+    localStorage.clear();
+})
+//evento change para elegor cuotas
+const cuotas = document.querySelector('#cuotas');
+cuotas.addEventListener('change', () => {
+    valorCuota = cuotas.value.toLowerCase();
+    console.log(valorCuota);
+    //funcion para elegir cuotas
+    const seleccionCuotas = () => {
+        if (valorCuota === 'una') {
+            totalCuota = totalPagar / 1;
+            document.querySelector('#cuotaSeleccionada').innerText = `Usted eligió 1 cuota sin interes, el valor de su cuota es de $ ${totalCuota}`;
+        } else if (valorCuota === 'tres') {
+            totalCuota = (totalPagar * 1.25) / 3;
+            document.querySelector('#cuotaSeleccionada').innerText = `Usted eligió 1 cuota sin interes, el valor de su cuota es de $ ${totalCuota}`;
+        } else {
+            totalCuota = (totalPagar * 1.40) / 6;
+            document.querySelector('#cuotaSeleccionada').innerText = `Usted eligió 1 cuota sin interes, el valor de su cuota es de $ ${totalCuota}`;
+        }
+    }
+    seleccionCuotas();
+    console.log(parseInt(totalCuota));
 })
 
 //formulario de contacto
 const formulario = document.querySelector('.form-contacto')
 const nombre = document.querySelector('#nombre')
-    const email = document.querySelector('#email')
-    const telefono = document.querySelector('#telefono')
-    const textarea = document.querySelector('#consulta')
-formulario.addEventListener('submit', (e)=>{
-    
+const email = document.querySelector('#email')
+const telefono = document.querySelector('#telefono')
+const textarea = document.querySelector('#consulta')
+formulario.addEventListener('submit', (e) => {
+
     e.preventDefault();
-    
+
     console.log(nombre.value)
     console.log(email.value)
     console.log(telefono.value)
     console.log(textarea.value)
 
-    
+
     //almacenar informacion de formulario
     localStorage.setItem('nombre', JSON.stringify(nombre.value));
 
-    
+
     //acceder a la informacion
     let nombreAlmacenado = JSON.parse(localStorage.getItem('nombre'));
 
@@ -316,9 +302,6 @@ formulario.addEventListener('submit', (e)=>{
     p.classList.add('saludo')
     document.body.append(p)
 });
-   
-//mensaje de saludo al terminar
-    
 
 //agrego h2
 const h2 = document.getElementById('h2');
@@ -334,7 +317,7 @@ for (const viaje of tramos) {
     <p> ${viaje.at} Hs</p>
     <h5> $ ${viaje.price} /persona</h5> 
     </div>`
- divCard.append(divServ)
+    divCard.append(divServ)
 }
 
 //agrego un h5 con terminos y condiciones
@@ -347,7 +330,7 @@ servicios.append(h5)
 //en los parrafos existentes de terminos y condiciones agrego contenido
 let terminoClases = document.getElementsByClassName('terminos__texto')
 for (const termino of terminoClases) {
-    console.log(termino.innerHTML)
+
 }
 
 terminoClases[0].innerText = 'Los menores de 2 años no pagan pasaje y deben ir acompañados de 1 adulto.'
